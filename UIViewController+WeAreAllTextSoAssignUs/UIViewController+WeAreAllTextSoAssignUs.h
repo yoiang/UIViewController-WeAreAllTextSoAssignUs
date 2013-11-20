4701a32a934f8dde70ef18dbd3b0892455298e96
Ian G <yo.ian.g@gmail.com>
2013-11-20 13:12:48 -0500

    refactoring

--- UIViewController+WeAreAllTextSoAssignUs/UIViewController+WeAreAllTextSoAssignUs.h
+++ UIViewController+WeAreAllTextSoAssignUs/UIViewController+WeAreAllTextSoAssignUs.h
@@ -10,10 +10,14 @@
 
 @interface UIViewController (WeAreAllTextSoAssignUs)
 
-- (void)setText:(NSString *)text toLabel:(UILabel *)label andButton:(UIButton *)button andTextField:(UITextField *)textField andTextView:(UITextView *)textView;
-- (void)setText:(NSString *)text toViews:(NSArray *)views;
-
 - (void)mapViews:(NSArray *)views forKey:(NSString *)key;
+
+- (void)setText:(NSString *)text toViews:(NSArray *)views;
 - (void)setText:(NSString *)text toViewsWithKey:(NSString *)key;
+- (void)setText:(NSString *)text
+        toLabel:(UILabel *)label
+      andButton:(UIButton *)button
+   andTextField:(UITextField *)textField
+    andTextView:(UITextView *)textView;
 
 @end
--- UIViewController+WeAreAllTextSoAssignUs/UIViewController+WeAreAllTextSoAssignUs.m
+++ UIViewController+WeAreAllTextSoAssignUs/UIViewController+WeAreAllTextSoAssignUs.m
@@ -16,48 +16,64 @@
 
 @implementation UIViewController (WeAreAllTextSoAssignUs)
 
-- (void)setText:(NSString *)text toLabel:(UILabel *)label andButton:(UIButton *)button andTextField:(UITextField *)textField andTextView:(UITextView *)textView
-{
-    [self setText:text toViews:@[label, button, textField, textView] ];
-}
-
-- (void)setText:(NSString *)text toViews:(NSArray *)views
-{
-    for (id view in views)
-    {
-        if ( [view isKindOfClass:[UILabel class] ] )
-        {
-            UILabel *label = (UILabel *)view;
-            label.text = text;
-        } else if ( [view isKindOfClass:[UIButton class] ] )
-        {
-            UIButton *button = (UIButton *)view;
-            button.titleLabel.text = text;
-        } else if ( [view isKindOfClass:[UITextField class] ] )
-        {
-            UITextField *textField = (UITextField *)view;
-            textField.text = text;
-        } else if ( [view isKindOfClass:[UITextView class] ] )
-        {
-            UITextView *textView = (UITextView *)view;
-            textView.text = text;
-        }
-    }
-}
-
 - (void)mapViews:(NSArray *)views forKey:(NSString *)key
 {
     [self.viewGroups setObject:views forKey:key];
 }
 
-- (void)setText:(NSString *)text toViewsWithKey:(NSString *)key
+- (void)callOnViews:(void (^)(NSArray *views) )callBlock forKey:(NSString *)key
 {
     id viewsId = [self.viewGroups objectForKey:key];
     if (viewsId && [viewsId isKindOfClass:[NSArray class] ] )
     {
         NSArray *views = (NSArray *)viewsId;
-        [self setText:text toViews:views];
+        callBlock( views );
     }
+}
+
++ (void)setText:(NSString *)text toView:(UIView *)view
+{
+    if ( [view isKindOfClass:[UILabel class] ] )
+    {
+        UILabel *label = (UILabel *)view;
+        label.text = text;
+    } else if ( [view isKindOfClass:[UIButton class] ] )
+    {
+        UIButton *button = (UIButton *)view;
+        button.titleLabel.text = text;
+    } else if ( [view isKindOfClass:[UITextField class] ] )
+    {
+        UITextField *textField = (UITextField *)view;
+        textField.text = text;
+    } else if ( [view isKindOfClass:[UITextView class] ] )
+    {
+        UITextView *textView = (UITextView *)view;
+        textView.text = text;
+    }
+}
+
+- (void)setText:(NSString *)text toViews:(NSArray *)views
+{
+    [views enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        [UIViewController setText:text toView:obj];
+    } ];
+}
+
+- (void)setText:(NSString *)text toViewsWithKey:(NSString *)key
+{
+    [self callOnViews:^(NSArray *views) {
+        [self setText:text toViews:views];
+    } forKey:key];
+}
+
+- (void)setText:(NSString *)text
+        toLabel:(UILabel *)label
+      andButton:(UIButton *)button
+   andTextField:(UITextField *)textField
+    andTextView:(UITextView *)textView
+{
+    [self setText:text toViews:@[label, button, textField, textView] ];
+}
 }
 
 @end
